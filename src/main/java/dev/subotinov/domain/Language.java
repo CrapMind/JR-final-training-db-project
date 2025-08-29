6f2930ad0e9eba5c3d0e332b3f67c14c9739902d
Yura Subotinov <crapmindd@gmail.com>
2025-08-29 15:00:13 +0300

    chore: setup DB connection and entity mappings

--- src/main/java/dev/subotinov/domain/Language.java
+++ src/main/java/dev/subotinov/domain/Language.java
@@ -1,4 +1,24 @@
 package dev.subotinov.domain;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table (schema = "world", name = "country_language")
 public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+    @Column(name = "language", length = 30, nullable = false)
+    private String language;
+    @Column(name = "is_official", columnDefinition = "BIT", nullable = false)
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isOfficial;
+    @Column(name = "percentage", nullable = false, columnDefinition = "decimal(4,1)")
+    private BigDecimal percentage;
 }
